663d8eb4fffe5ae74f019f1ece25232ae58afc38
mmajd <mabulmagd55@gmail.com>
2022-05-28 23:53:41 +0200

    Monotonic stack, leetcode no. 84

---
+++ MonotonicStack.java
@@ -0,0 +1,65 @@
+/**
+ * Monotnoic Stack contains element which are either increasing only 
+ * or decreasing only manner 
+ *
+ * MonotonicStack.java
+ * Solution for leetcode problem no 84
+ * Find largest rectangle area in some histogram 
+ *
+ * In this solution, we use increasing MS
+ * as we want to find the least prev/next least element 
+ * for each element in the heights list 
+ *
+ * */
+
+
+private class Solution {
+    public int largestRectangleArea(int[] heights) {
+        int ans = 0;
+        Deque<Integer> stack = null;
+        
+        int[] PLE = new int[heights.length];
+        stack = new LinkedList<Integer>();
+        
+        for(int i=0 ; i< heights.length;i++) {
+            while(stack.size()>0 && heights[stack.peek()] >= heights[i])
+                stack.pop();
+            
+            if(stack.size()==0) {
+                PLE[i] = -1;
+            } else {
+                PLE[i] = stack.peek();
+            }
+            
+            stack.push(i);
+        }
+        
+        int[] NLE = new int[heights.length];
+        stack = new LinkedList<Integer>();
+        
+        for(int i = heights.length - 1 ; i >=0 ; i--) {
+            while(stack.size()>0 && heights[stack.peek()] >= heights[i])
+                stack.pop();
+            
+            if(stack.size()==0) NLE[i] = heights.length;
+            else NLE[i] = stack.peek();
+            
+            stack.push(i);
+        }
+        
+        for(int i = 0 ; i < heights.length ; i++){
+            int temp = (NLE[i] - PLE[i] - 1) * heights[i];
+            ans = Math.max(ans , temp);
+        }
+        return ans;
+        
+    }
+}
+
+
+public class MonotonicStack { 
+	public static void main(String[] args) { 
+		/** */ 
+	}
+}
+
