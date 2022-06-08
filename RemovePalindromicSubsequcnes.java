31bb7cc1f51d6913a045298c9659f31039d7e840
mmajd <mabulmagd55@gmail.com>
2022-06-08 20:56:41 +0200

    remove palindromic subsequences from 2 chars string

--- RemovePalindromicSubsequcnes.java
+++ RemovePalindromicSubsequcnes.java
@@ -3,7 +3,7 @@
  *
  * NOTE: The string consist only of two characters (ex: a, b) 
  *
- * Subsequence doesn't mean the a contingous 
+ * Subsequence doesn't mean contingous, 
  * thus there're two solutions, first one string is a palindrome and it takes us 1 step to remove it 
  * second one string is not palindrome, here we take one step to remove all occurences of the first character 
  * and another step to the remaining palindrome (one character string) 
