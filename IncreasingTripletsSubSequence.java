6168fd76ae885860fc3dc89c1a865d2eaf0d3f06
mmajd <mabulmagd55@gmail.com>
2022-06-15 15:25:30 +0200

    334. O(n) space 1

--- IncreasingTripletsSubSequence.java
+++ IncreasingTripletsSubSequence.java
@@ -3,6 +3,22 @@
  */
 
 class IncreasingTripletsSubSequence {
+    /** n solution */
+    class Solution {
+        public boolean increasingTriplet(int[] nums) {
+            int left= Integer.MAX_VALUE, right= Integer.MAX_VALUE;
+            //take assumption : left < right
+            for(int n: nums){
+                //Test each number one by one comparing First with left and then with right
+                if(n<=left) left= n;
+                else if(n<=right) right= n;  // If n is LTET right, means n is surely GT left
+                else return true; // If n is GTET left and GTET right that means we got the increasing triplet
+            }
+            return false;
+        }
+    }
+
+
     /** binary search solution */
      class Solution {
          public boolean increasingTriplet(int[] nums) {
