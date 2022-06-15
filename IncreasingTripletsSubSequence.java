5be67ee3d214dd614f71f82252bd68a0b563bdd9
mmajd <mabulmagd55@gmail.com>
2022-06-15 15:23:12 +0200

    334. binary search solution

--- IncreasingTripletsSubSequence.java
+++ IncreasingTripletsSubSequence.java
@@ -3,6 +3,29 @@
  */
 
 class IncreasingTripletsSubSequence {
+    /** binary search solution */
+     class Solution {
+         public boolean increasingTriplet(int[] nums) {
+             int[] dp= new int[3];
+             int len=0;
+             for(int n: nums){
+                 // returns -1 * (insertion point - 1) 
+                 int index= Arrays.binarySearch(dp,0,len, n);
+                 if(index<0){
+                     index=-(index+1); // to get insertion index;
+                 }
+              
+                 dp[index]= n;
+
+                 if(index == len)               
+                     len++;
+                 if(len==3) return true;
+             }
+             return false;
+         }
+     }
+
+
 
     public boolean increasingTriplet(int[] nums) {
         int len = nums.length; 
