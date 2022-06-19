493d73f11e38323a7892651c552efb554e3ef8f3
mmajd <mabulmagd55@gmail.com>
2022-06-19 07:54:59 +0200

    Populating next right pointers in each node

--- PopulatingNextRightPointersinEachNode.java
+++ PopulatingNextRightPointersinEachNode.java
@@ -1,8 +1,7 @@
-/** 
- *
- */ 
-
 /*
+ * @link https://leetcode.com/problems/populating-next-right-pointers-in-each-node/
+ *
+ *
 // Definition for a Node.
 class Node {
     public int val;
@@ -27,14 +26,34 @@ class Node {
 
 class Solution {
     public Node connect(Node root) {
-        if(root == null) return root;
+        if(root ==null) return root;
         
         Node curr = root;
         
-        dfs(curr.left, curr.right);
+        // dfs(curr.left, curr.right);
+        bfs(curr);
         
         return root;
     }
+    
+    public void bfs(Node curr) { 
+        Node next = curr.left; 
+        
+        while(curr != null && next != null) {
+            curr.left.next = curr.right;
+            
+            if (curr.next != null) {
+                curr.right.next = curr.next.left;
+            }
+            
+            curr = curr.next;
+            if (curr == null) { 
+                curr = next; 
+                next = curr.left; 
+            }
+        }
+    }
+   
     
     public void dfs(Node n1, Node n2) {
         if(n1 == null || n2 == null)
@@ -47,4 +66,14 @@ class Solution {
         dfs(n2.left, n2.right);
     }
 }
+
+
+
+
+
+
+
+
+
+
 
