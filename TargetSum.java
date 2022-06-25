c87c70e416a3769a0765983ba2d85e2067ca7b8c
mmajd <mabulmagd55@gmail.com>
2022-06-25 09:07:22 +0200

    Target sum

---
+++ TargetSum.java
@@ -0,0 +1,26 @@
+
+/** 
+ *
+ * @link https://leetcode.com/problems/target-sum
+ *
+ * There's no need to use memoization as our input range is small in all inputs,
+ * thus using DFS is acceptal
+ */
+
+class Solution {
+    public int findTargetSumWays(int[] nums, int target) {
+        return dfs(nums, 0, target);
+    }
+    
+    private int dfs(int[] nums, int i, int target) { 
+        if (i >= nums.length) { 
+            return target == 0 ? 1 : 0; 
+        }
+        
+        int pos = dfs(nums, i+1, target+nums[i]);
+        int neg = dfs(nums, i+1, target-nums[i]);
+        
+        return pos + neg;
+    }
+}
+
