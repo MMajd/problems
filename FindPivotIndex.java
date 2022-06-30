d1c3fd52e4faf534dd7c7d8861700baf9680a786
mmajd <mabulmagd55@gmail.com>
2022-06-30 11:16:47 +0200

    find pivot index

--- FindPivotIndex.java
+++ FindPivotIndex.java
@@ -2,9 +2,23 @@
 /** 
  * @ https://leetcode.com/problems/find-pivot-index/
  */ 
-
 class Solution {
-    public int pivotIndex(int[] nums) {
+    public int pivotIndex(int[] nums) { 
+        int len = nums.length; 
+        int sum = 0, left = 0; 
+        
+        for (int i : nums) sum += i; 
+        
+        for (int i=0; i<len; i++) { 
+            if (left == sum - left - nums[i]) return i; 
+            left += nums[i]; 
+        }
+        
+        return -1; 
+    }
+    
+    
+    public int pivotIndex2(int[] nums) {
         int len = nums.length; 
         
         if (len == 1) return 0; 
