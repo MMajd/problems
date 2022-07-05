4c6d12bd93debae2153f50627c730b06a58ca6ea
mmajd <mabulmagd55@gmail.com>
2022-07-05 08:50:48 +0200

    Reverse string

---
+++ ReverseString.java
@@ -0,0 +1,22 @@
+
+/**
+ *
+ * @link https://leetcode.com/problems/reverse-string/
+ *
+ *
+ */
+
+class Solution {
+    public void reverseString(char[] s) {
+        int i = 0; 
+        int j = s.length-1; 
+        
+        while(i < j) { 
+            char x = s[i]; 
+            s[i]  = s[j]; 
+            s[j] = x; 
+            i++; 
+            --j; 
+        }
+    }
+}
