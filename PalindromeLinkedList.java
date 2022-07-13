8d77913474ea825861bf1b0eb93ef96185d2de8e
mmajd <mabulmagd55@gmail.com>
2022-07-13 11:46:24 +0200

    palindrome linkedlist

--- PalindromeLinkedList.java
+++ PalindromeLinkedList.java
@@ -2,6 +2,24 @@
   @link https://leetcode.com/problems/palindrome-linked-list
 
   To be solved need to know reverse list algo & slow-fast-pointers alog 
+
+
+Problem Stmt:
+  Given the head of a singly linked list, return true if it is a palindrome.
+
+
+Example 1:
+    Input: head = [1,2,2,1]
+    Output: true
+
+Example 2:
+    Input: head = [1,2]
+    Output: false
+
+Constraints:
+    The number of nodes in the list is in the range [1, 10^5].
+    0 <= Node.val <= 9
+
 */ 
 
 
