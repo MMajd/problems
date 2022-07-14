0aa8bcd159b2e0d88d620296566b1d45eb748ed6
mmajd <mabulmagd55@gmail.com>
2022-07-14 17:59:18 +0200

    reverse string recursive approach

--- ReverseString.java
+++ ReverseString.java
@@ -7,16 +7,28 @@
  */
 
 class Solution {
-    public void reverseString(char[] s) {
+    public void reverseString(char[] s) { 
+        reverse(s, 0); 
+    }
+
+    public void reverse(char[] s, int i) { 
+        if (i >= s.length/2) return;
+        reverse(s, i+1); 
+        swap(s, s.length-1-i, i); 
+    }
+
+    public void reverseStringIterative(char[] s) {
         int i = 0; 
         int j = s.length-1; 
         
         while(i < j) { 
-            char x = s[i]; 
-            s[i]  = s[j]; 
-            s[j] = x; 
-            i++; 
-            --j; 
+            swap(s, i++, j--);
         }
+    }
+
+    public void swap(char[] s, int i, int j) { 
+        char x = s[i]; 
+        s[i]  = s[j]; 
+        s[j] = x; 
     }
 }
