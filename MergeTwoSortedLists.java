80cb45550d0871f63df963eed030755e3ad6957c
mmajd <mabulmagd55@gmail.com>
2022-07-16 11:48:15 +0200

    merge two sorted lists using recursive approach

--- MergeTwoSortedLists.java
+++ MergeTwoSortedLists.java
@@ -2,22 +2,22 @@
   
  @link https://leetcode.com/problems/merge-two-sorted-lists
   
- You are given the heads of two sorted linked lists list1 and list2.
+ You are given the heads of two sorted linked lists l1 and l2.
 
  Merge the two lists in a one sorted list. The list should be made by splicing together the nodes of the first two lists.
 
  Return the head of the merged linked list.
 
  Example 1:
-    Input: list1 = [1,2,4], list2 = [1,3,4]
+    Input: l1 = [1,2,4], l2 = [1,3,4]
     Output: [1,1,2,3,4,4]
 
 Example 2:
-    Input: list1 = [], list2 = []
+    Input: l1 = [], l2 = []
     Output: []
 
 Example 3:
-    Input: list1 = [], list2 = [0]
+    Input: l1 = [], l2 = [0]
     Output: [0]
 
 
@@ -26,11 +26,10 @@ Example 3:
 Constraints:
     The number of nodes in both lists is in the range [0, 50].
     -100 <= Node.val <= 100
-    Both list1 and list2 are sorted in non-decreasing order.
+    Both l1 and l2 are sorted in non-decreasing order.
 
   
  */ 
-
 
 /**
  * Definition for singly-linked list.
@@ -43,16 +42,31 @@ Constraints:
  * }
  */
 class Solution {
-    public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
-        if (list1 == null || list2 == null) { 
-            return list1 == null ? list2 : list1; 
+    
+    public ListNode mergeTwoListsRecursive(ListNode l1, ListNode l2) {
+        if (l1 == null || l2 == null) { 
+            return l1 == null ? l2 : l1; 
         }
         
-        ListNode p1=list1, p2=list2, head=list1, prev1 = null; 
+        if (l1.val < l2.val)  { 
+            l1.next = mergeTwoLists(l1.next, l2);
+            return l1; 
+        }
+        
+        l2.next = mergeTwoLists(l1, l2.next);
+        return l2; 
+    }
+    
+    public ListNode mergeTwoListsIterative(ListNode l1, ListNode l2) {
+        if (l1 == null || l2 == null) { 
+            return l1 == null ? l2 : l1; 
+        }
+        
+        ListNode p1=l1, p2=l2, head=l1, prev1 = null; 
         
         if (p1.val > p2.val) { 
-            p1 = list2; 
-            p2 = list1; 
+            p1 = l2; 
+            p2 = l1; 
             head = p1; 
         }
         
