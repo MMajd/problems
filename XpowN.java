9948cd8713029065593b750e4f3b5c3ee7bf261c
mmajd <mabulmagd55@gmail.com>
2022-07-16 09:38:14 +0200

    myPow, x pow n using tail recusion

--- XpowN.java
+++ XpowN.java
@@ -1,8 +1,26 @@
-
 /** 
- *
- * @link : https://leetcode.com/problems/powx-n/ 
- */ 
+@link : https://leetcode.com/problems/powx-n/ 
+
+Implement pow(x, n), which calculates x raised to the power n (i.e., xn).
+
+Example 1:
+    Input: x = 2.00000, n = 10
+    Output: 1024.00000
+
+Example 2:
+    Input: x = 2.10000, n = 3
+    Output: 9.26100
+
+Example 3:
+    Input: x = 2.00000, n = -2
+    Output: 0.25000
+    Explanation: 2-2 = 1/22 = 1/4 = 0.25
+ 
+Constraints:
+    -100.0 < x < 100.0
+    -2^31 <= n <= 2^31-1
+    -10^4 <= x^n <= 10^4
+*/ 
 
 
 public class XpowN { 
@@ -19,8 +37,12 @@ public class XpowN {
         if (n < 0) { 
             x = 1.0 / x; 
 
-            if (power == Integer.MIN_VALUE) power = Integer.MAX_VALUE; 
-            else power *= -1; 
+            if (power == Integer.MIN_VALUE)  { 
+                power = Integer.MAX_VALUE; 
+            }
+            else { 
+                power *= -1; 
+            }
         }
         
         while (power != 0) { 
@@ -34,6 +56,28 @@ public class XpowN {
         
         return ans * sign; 
     }
+}
 
 
+class Solution {
+    public double myPow(double x, int n) {
+        int sign = ((x > 0) || (n%2==0)) ? 1 : -1;
+        x = Math.abs(x); 
+        
+        if (n < 0) {
+            x = 1/x;
+            
+            // convert power to positive value 
+            if (n == Integer.MIN_VALUE) n = Integer.MAX_VALUE;
+            else n *= -1; 
+        }
+        
+        return xPowN(x, n, 1*sign); 
+    }
+    
+    // tail recusion solution 
+    private double xPowN(double x, int n, double ans) { 
+        if (n==0) return ans; 
+        return xPowN(x, n-1, ans*x); 
+    }
 }
