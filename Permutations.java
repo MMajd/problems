eaa6cae62534d5a6636fe94d9c70c801ad460a00
mmajd <mabulmagd55@gmail.com>
2022-07-19 19:27:25 +0200

    add: another solution for permutations

--- Permutations.java
+++ Permutations.java
@@ -28,26 +28,22 @@ Constraints:
 
 
 class Solution {
-    int[] nums = null;
-    boolean[] used = null; 
     List<List<Integer>> ans = new ArrayList<>(); 
     public List<List<Integer>> permute(int[] nums) {
-        this.nums = nums; 
-        /* used to mark indices of used values
+        /* used array is used to mark indices of used values
          * marking indices as visited, is the same as marking values; 
          * values are unique, thus indices and values have a 
          * one-to-one mapping relation
         */ 
-        this.used = new boolean[nums.length]; 
+        boolean[] used = new boolean[nums.length]; 
         
-        solve(0, new ArrayList<>());
+        solve(0, nums, used, new ArrayList<>());
         
         return ans; 
     }
     
-    private void solve(int index, List<Integer> data) { 
+    private void solve(int index, nums, used, List<Integer> data) { 
         if (index == nums.length) { 
-            // ans.add(new ArrayList<>(Arrays.asList(data)));
             ans.add(new ArrayList<>(data));
             return; 
         }
@@ -60,5 +56,35 @@ class Solution {
             data.remove(data.size()-1);
             used[i] = false; 
         }
+    }
+}
+
+class Solution { 
+    List<List<Integer ans = new ArrayList<>(); 
+
+    public List<List<Integer>> permute(int[] nums) { 
+        solve(0, nums, new ArrayList<>());
+        return ans; 
+    }
+
+    private void solve(int index, int[] nums, List<Integer> data) { 
+        if (index == nums.length) { 
+            ans.add(new ArrayList<>(data));
+            return; 
+        }
+
+        for (int i=index; i<nums.length; i++) { 
+            swap(i, index, nums);
+            data.add(nusm[i]);
+            solve(index+1, nums, data); 
+            data.remove(data.size()-1);
+            swap(i, index, nums);
+        }
+    }
+
+    private void swap(int i, int j, int[] arr) { 
+        int temp = arr[i]; 
+        arr[i] = arr[j]; 
+        arr[j] = temp; 
     }
 }
