0a0afb4918ab5e014393969d99ced481988b7bb7
mmajd <mabulmagd55@gmail.com>
2022-07-26 06:44:54 +0200

    subsets with counting sort

--- SubsetsII.java
+++ SubsetsII.java
@@ -88,3 +88,67 @@ class Solution {
         }
     }
 }
+
+/** Using counting sort */
+class Solution {
+    List<List<Integer>> ans = new LinkedList<>(); 
+    public List<List<Integer>> subsetsWithDup(int[] nums) {
+        sort(nums); 
+        
+        for (int i=0; i<=nums.length; i++) { 
+            solve(0, i, nums, new ArrayList<>(nums.length));
+        }
+        
+        return ans; 
+    }
+    
+    private void solve(int s, int len, int[] nums, List<Integer> data) { 
+        if (len == data.size()) { 
+            ans.add(new ArrayList<>(data));
+            return;
+        }
+        
+        // Set<Integer> set = new HashSet<>();
+        Integer prev = null; 
+        
+        for (int i=s; i<nums.length; i++) { 
+            // if (set.contains(nums[i])) continue; 
+            if (prev != null && prev == nums[i]) continue; 
+            prev = nums[i];
+            data.add(nums[i]);
+            solve(i+1, len, nums, data); 
+            data.remove(data.size()-1);
+            // set.add(nums[i]);
+        }
+    }
+    
+    public void sort(int[] arr) { 
+        int min = 20;  
+        int max = -20;
+        
+        for (int i : arr) { 
+            if (min > i) min = i; 
+            if (i > max) max = i; 
+        }
+        
+        int[] output = new int[arr.length];
+        int[] count = new int[max-min+1];
+        
+        for (int i=0; i<arr.length; i++) { 
+            count[arr[i]-min] += 1;
+        }
+        
+        for (int i=1; i<count.length; i++) { 
+            count[i] += count[i-1];
+        }
+        
+        for (int i=0; i<arr.length; i++) { 
+            output[count[arr[i]-min]-1] = arr[i]; 
+            count[arr[i]-min]--; 
+        }
+        
+        for (int i=0; i<arr.length; i++) { 
+            arr[i] = output[i]; 
+        }
+    }
+}
