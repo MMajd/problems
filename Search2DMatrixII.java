d75f0459a43f2c0568a7929e1e1db1283b1ff860
mmajd <mabulmagd55@gmail.com>
2022-07-28 10:27:34 +0200

    adding my sol link on lc

--- Search2DMatrixII.java
+++ Search2DMatrixII.java
@@ -2,6 +2,9 @@
 
 @link https://leetcode.com/problems/search-a-2d-matrix-ii/
 
+My Sol. on LC: https://leetcode.com/problems/search-a-2d-matrix-ii/discuss/2344898/Java-Decrease-and-Conquer-Search-In-Upper-Right-and-Bottom-Left
+
+
 Write an efficient algorithm that searches for a value target in an m x n integer matrix matrix. This matrix has the following properties:
 
 Integers in each row are sorted in ascending from left to right.
