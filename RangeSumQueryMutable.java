54fa47b0ad766212e68e1b5ae7c6d26411381985
mmajd <mabulmagd55@gmail.com>
2022-07-31 20:25:18 +0200

    update sum query range mutable

--- RangeSumQueryMutable.java
+++ RangeSumQueryMutable.java
@@ -1,67 +1,112 @@
 /** 
- * This solution depends on segTree tree
- * @link: https://leetcode.com/problems/range-sum-query-mutable
+@link: https://leetcode.com/problems/range-sum-query-mutable
+
+Given an integer array nums, handle multiple queries of the following types:
+
+Update the value of an element in nums.
+Calculate the sum of the elements of nums between indices left and right inclusive where left <= right.
+Implement the NumArray class:
+
+NumArray(int[] nums) Initializes the object with the integer array nums.
+void update(int index, int val) Updates the value of nums[index] to be val.
+int sumRange(int left, int right) Returns the sum of the elements of nums between 
+indices left and right inclusive (i.e. nums[left] + nums[left + 1] + ... + nums[right]).
+
+
+Example 1:
+    Input
+    ["NumArray", "sumRange", "update", "sumRange"]
+    [[[1, 3, 5]], [0, 2], [1, 2], [0, 2]]
+    Output
+    [null, 9, null, 8]
+
+Explanation
+    NumArray numArray = new NumArray([1, 3, 5]);
+    numArray.sumRange(0, 2);    
+    return 1 + 3 + 5 = 9
+    numArray.update(1, 2);   // nums = [1, 2, 5]
+    numArray.sumRange(0, 2); // return 1 + 2 + 5 = 8
+
+                             
+Constraints:
+    1 <= nums.length <= 3 * 104
+    -100 <= nums[i] <= 100
+    0 <= index < nums.length
+    -100 <= val <= 100
+    0 <= left <= right < nums.length
+    At most 3 * 104 calls will be made to update and sumRange.
+  
  */
-public class RangeQueryMutable { 
 
-    public static void main(String[] args) {}
+class NumArray {
+    private int[] seg, arr; 
+    private int n; 
 
-    private class NumArray {
-
-        int[] segTree;
-        int n;
+    public NumArray(int[] nums) {
+        n = nums.length; 
+        seg = new int[4*n]; 
+        arr = nums.clone(); 
         
-        public NumArray(int[] nums) {
-            this.n = nums.length;
-            this.segTree = new int[4 * n];
-            build(nums, 1, 0, n - 1);
-        }
-        
-        private void build(int[] nums, int pos, int start, int end) {
-            if(start == end){
-                this.segTree[pos] = nums[start];
-                return;
-            }
-            
-            int mid = start + (end - start) / 2;
-            build(nums, 2 * pos, start, mid);
-            build(nums, 2 * pos + 1, mid + 1, end);
-            
-            segTree[pos] = segTree[2 * pos] + segTree[2 * pos + 1];
+        build(1, 0, n-1); 
+    }
+
+    private void build(int pos, int start, int end) { 
+        if (start == end) { 
+            seg[pos] = arr[start]; 
+            return; 
         }
         
-        public void update(int index, int val) {
-            updateHelper(1, 0, n - 1, index, val);
+        int mid = start+(end-start)/2; 
+        
+        build(2*pos, start, mid); 
+        build(2*pos+1, mid+1, end); 
+        
+        seg[pos] = seg[2*pos] + seg[2*pos+1]; 
+    }
+    
+    private void updateHelper(int pos, int start, int end, 
+                              int idx, int val) { 
+        
+        if (start == end) { 
+            seg[pos] = val;
+            return; 
         }
         
-        private void updateHelper(int pos, int start, int end, int idx, int val) {
-            if(start == end) {
-                segTree[pos] = val;
-                return; 
-            }
-            
-            int mid = start + (end - start) / 2;
-            if(idx > mid) updateHelper(2 * pos + 1, mid + 1, end, idx, val);
-            else updateHelper(2 * pos, start, mid, idx, val);
-
-            segTree[pos] = segTree[2 * pos] + segTree[2 * pos + 1]; 
+        int mid = start+(end-start)/2; 
+        
+        if (idx>mid) updateHelper(2*pos+1, mid+1, end, idx, val); 
+        else updateHelper(2*pos, start, mid, idx, val); 
+        
+        seg[pos] = seg[2*pos] + seg[2*pos+1]; 
+    }
+    
+    public void update(int index, int val) {
+        updateHelper(1, 0, n-1, index, val); 
+    }
+    
+    
+    public int get(int pos, int start, int end, int left, int right) { 
+        if (start>=left && end<=right) { 
+            return seg[pos]; 
         }
-         
-        private int sumRangeHelper(int pos, int start, int end, int left, int right) {
-            if(left <= start && right >= end)
-                return segTree[pos];
-            
-            if(left > end || right < start)
-                return 0;
-            
-            int mid = start + (end - start) / 2;
-            int l = sumRangeHelper(2 * pos, start, mid, left, right);
-            int r = sumRangeHelper(2 * pos + 1, mid + 1, end, left, right);
-            return l + r;
-        }
-
-        public int sumRange(int left, int right) {
-            return sumRangeHelper(1, 0, n - 1, left, right);
-        }
+        
+        if (start>right || end<left) return 0; 
+        
+        int mid = start+(end-start)/2; 
+        int l = get(2*pos, start, mid, left, right); 
+        int r = get(2*pos+1, mid+1, end, left, right); 
+        
+        return l + r; 
+    }
+    
+    public int sumRange(int left, int right) {
+        return get(1, 0, n-1, left, right); 
     }
 }
+
+/**
+ * Your NumArray object will be instantiated and called as such:
+ * NumArray obj = new NumArray(nums);
+ * obj.update(index,val);
+ * int param_2 = obj.sumRange(left,right);
+ */
