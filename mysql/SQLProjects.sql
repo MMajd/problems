9fbcf3868dcd356aa2c319ee7a8713f24e2a0a11
mmajd <mabulmagd55@gmail.com>
2022-08-12 15:52:56 +0200

    hackerrank placements

--- mysql/SQLProjects.sql
+++ mysql/SQLProjects.sql
@@ -8,15 +8,14 @@ full description
 /** deactivate full group by mode */
 set sql_mode = ''; 
 SELECT Start_Date, End_Date
-FROM (
-        /** select start date that has not appeard as an end date for some task, true start date of a project */ 
-    (SELECT Start_Date FROM Projects WHERE Start_Date NOT IN (SELECT End_Date FROM Projects)) a,
-    /** select end date that has not appeard as a start date for some task, true end date of a project*/
-    (SELECT End_Date FROM Projects WHERE End_Date NOT IN (SELECT Start_Date FROM Projects)) b 
-)
+    FROM (
+            /** select start date that has not appeard as an end date for some task, true start date of a project */ 
+        (SELECT Start_Date FROM Projects WHERE Start_Date NOT IN (SELECT End_Date FROM Projects)) AS A,
+        /** select end date that has not appeard as a start date for some task, true end date of a project*/
+        (SELECT End_Date FROM Projects WHERE End_Date NOT IN (SELECT Start_Date FROM Projects)) AS B 
+    )
 /** make sure that dates are valid */
 WHERE Start_Date < End_Date
-GROUP BY Start_Date 
+GROUP BY Start_Date
 /** order by days taken asc, if equal order by early started project */
 ORDER BY DATEDIFF(End_Date, Start_Date), Start_Date
-
---
+++ mysql/StudentsAndFriendWithHigherOffers.sql
@@ -0,0 +1,17 @@
+/**
+    @link https://www.hackerrank.com/challenges/placements/problem?isFullScreen=true
+
+You are given three tables: Students, Friends and Packages. Students contains two columns: ID and Name. Friends contains two columns: ID and Friend_ID (ID of the ONLY best friend). Packages contains two columns: ID and Salary (offered salary in $ thousands per month).
+
+
+Write a query to output the names of those students whose best friends got offered a higher salary than them. Names must be ordered by the salary amount offered to the best friends. It is guaranteed that no two students got same salary offer.
+
+*/
+
+SELECT S.Name FROM (Students S 
+    JOIN Friends F   ON S.ID=F.ID 
+    JOIN Packages P1 ON S.ID=P1.ID
+    JOIN Packages P2 ON F.Friend_ID=P2.ID
+    )
+WHERE P2.Salary > P1.Salary 
+Order By P2.Salary;
