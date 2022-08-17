0ae57ef28dcfe03f2f4e49b4b8ff2f0d39887d9f
mmajd <mabulmagd55@gmail.com>
2022-08-17 08:02:25 +0200

    find duplicate number

---
+++ FindDuplicateNumber.java
@@ -0,0 +1,87 @@
+/*
+
+ @link https://leetcode.com/problems/find-the-duplicate-number/
+
+
+Given an array of integers nums containing n + 1 integers where each integer is in the range [1, n] inclusive.
+
+There is only one repeated number in nums, 
+return this repeated number.
+
+You must solve the problem without modifying the array nums and uses only constant extra space.
+
+ 
+
+Example 1:
+    Input: nums = [1,3,4,2,2]
+    Output: 2
+
+Example 2:
+    Input: nums = [3,1,3,4,2]
+    Output: 3
+
+Constraints:
+    1 <= n <= 10^5
+    nums.length == n + 1
+    1 <= nums[i] <= n
+    All the integers in nums appear only once except for precisely one integer which appears two or more times.
+ 
+
+Follow up:
+    How can we prove that at least one duplicate number must exist in nums?
+    Can you solve the problem in linear runtime complexity?
+
+*/
+
+
+
+
+/** 
+ * Loop Detection
+ * Floyd's Tortise & Hare [Fast & Slow], 
+ * */
+class Solution {
+    public int findDuplicate(int[] arr) {
+        int fast = arr[0]; 
+        int slow = arr[0]; 
+
+        do {
+            slow = arr[slow]; 
+            fast = arr[arr[fast]]; 
+        } while(fast != slow);
+
+        slow = arr[0]; 
+
+        while(slow != fast) { 
+            slow = arr[slow]; 
+            fast = arr[fast]; 
+        }
+
+        return slow; 
+    }
+}
+
+
+
+/** Maybe violating the constraints */
+class Solution {
+    public int findDuplicate(int[] nums) {
+        int duplicate = -1; 
+        
+        for (int i=0; i<nums.length; i++) { 
+            int curr = Math.abs(nums[i]);
+            if (nums[curr] < 0) { 
+                duplicate=curr; 
+                break;
+            }
+                
+            nums[curr] *= -1; 
+        }
+        
+        for (int i=0; i<nums.length; i++) { 
+            nums[i] = Math.abs(nums[i]);
+        }
+        
+        return duplicate; 
+    }
+}
