14ca20a99339cb94ac46bf7c8e1363b1091d794d
mmajd <mabulmagd55@gmail.com>
2022-08-29 11:43:33 +0200

    (map/sorting) group anagrams

--- GroupAnagrams.java
+++ GroupAnagrams.java
@@ -5,7 +5,6 @@
 
   An Anagram is a word or phrase formed by rearranging the letters of a different word or phrase, typically using all the original letters exactly once.
 
-
 Example 1:
     Input: strs = ["eat","tea","tan","ate","nat","bat"]
     Output: [["bat"],["nat","tan"],["ate","eat","tea"]]
