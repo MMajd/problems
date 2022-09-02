63e9eab419eaf82c933094697f1edc73aad2093e
mmajd <mabulmagd55@gmail.com>
2022-09-02 17:23:34 +0200

    (sliding window/map) Longest Substring Without Repeating Characters

--- LongestSubStringWithNonRepeatingCharacters.java
+++ LongestSubStringWithNonRepeatingCharacters.java
@@ -1,24 +1,56 @@
-/** 
- *
- * Sliding window solution 
- *
- * @link https://leetcode.com/problems/longest-substring-without-repeating-characters/
- *
+/*  
+  @link https://leetcode.com/problems/longest-substring-without-repeating-characters/
+  
+  Given a string s, find the length of the longest substring without repeating characters.
+
+Example 1:
+    Input: s = "abcabcbb"
+    Output: 3
+    Explanation: The answer is "abc", with the length of 3.
+
+Example 2:
+    Input: s = "bbbbb"
+    Output: 1
+    Explanation: The answer is "b", with the length of 1.
+
+Example 3:
+    Input: s = "pwwkew"
+    Output: 3
+    Explanation: The answer is "wke", with the length of 3.
+    Notice that the answer must be a substring, "pwke" is a subsequence and not a substring.
+
+Constraints:
+    0 <= s.length <= 5 * 104
+    s consists of English letters, digits, symbols and spaces.
  */ 
 
-public class LongestSubStringWithNonRepeatingCharacters { 
-    /** 
-     * return the length of longest substring w/ non
-     * repeating characters 
-     * ex: abceabcabc -> solution len(abce) = 4 
-     */
+class Solution {
+    public int lengthOfLongestSubstring(String s) {
+        int[] chars = new int[127];
+        int i = 0; 
+        int j = 0; 
+        int ans = 0; 
+        
+        while(i<s.length()) { 
+            int code = (int) s.charAt(i);
+            if (chars[code] != 0) { 
+                j = Math.max(chars[code], j);
+            }
+            
+            ans = Math.max(ans, i-j+1);
+            chars[code] = ++i; 
+        }
+        
+        return ans; 
+    }
+}
 
-
-    public int solution(String s) { 
+/** With HashMap */
+public class Solution { 
+    public int lengthOfLongestSubstring(String s) { 
+        Map<Character, Integer> map = new HashMap<>(); 
         int maxlen = 0; 
         int j = 0; 
-
-        Map<Character, Integer> map = new HashMap<>(); 
 
         for (int i=0; i<s.length(); i++) { 
             if (map.containsKey(s.charAt(i)) { 
