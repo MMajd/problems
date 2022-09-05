4462867022b1cdb846949bdc684fd1ca03064539
mmajd <mabulmagd55@gmail.com>
2022-09-05 13:08:57 +0200

    (quickselect/map/heap) top k frequent elements in an array

--- TopKFrequentElements.java
+++ TopKFrequentElements.java
@@ -1,36 +1,113 @@
-/** 
- *
- * @link https://leetcode.com/problems/top-k-frequent-elements
- *
+/* 
+ @link https://leetcode.com/problems/top-k-frequent-elements
+
+ Given an integer array nums and an integer k, return the k most frequent elements. You may return the answer in any order.
+
+Example 1:
+    Input: nums = [1,1,1,2,2,3], k = 2
+    Output: [1,2]
+
+Example 2:
+    Input: nums = [1], k = 1
+    Output: [1]
+
+Constraints:
+    1 <= nums.length <= 105
+    -104 <= nums[i] <= 104
+    k is in the range [1, the number of unique elements in the array].
+    It is guaranteed that the answer is unique.
+ 
+
+ ****************
+ - Follow up: Your algorithm's time complexity must be better than O(n log n), where n is the array's size.
+ ****************
+  
  */
 
-class TopKFrequentElements {
+class Solution {
+    public void swap(int[] arr, int i, int j) {
+        int tmp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = tmp;
+    }
+
+    public int partition(int left, int right, int pidx, 
+                         int[] unique, Map<Integer,Integer> map) {
+        int pfreq = map.get(unique[pidx]);
+        swap(unique,pidx, right);
+        int j = left;
+
+        for (int i = left; i <= right; i++) {
+            if (map.get(unique[i]) < pfreq) {
+                swap(unique, j++, i);
+            }
+        }
+        
+        swap(unique, j, right);
+
+        return j;
+    }
+    
+    public void quickselect(int left, int right, int kidx, 
+                         int[] unique, Map<Integer,Integer> map) {
+        if (left == right) return;
+        int pidx = left + (right-left)/2;
+
+        pidx = partition(left, right, pidx, unique, map);
+
+        if (kidx == pidx) return;    
+        if (kidx < pidx) quickselect(left, pidx-1, kidx, unique, map);     
+        else quickselect(pidx + 1, right, kidx, unique, map);  
+    }
+    
     public int[] topKFrequent(int[] nums, int k) {
-        if (nums.length <= 1) return nums;
-        
-        Map<Integer, Integer> map = new HashMap<>();
-        
-        for (int n : nums) {
-            map.put(n, map.getOrDefault(n, 0) + 1);
+        Map<Integer, Integer> map = new HashMap();
+        for (int num: nums) {
+            map.put(num, map.getOrDefault(num, 0) + 1);
         }
         
-        PriorityQueue<Map.Entry<Integer,Integer>> q = new 
-            PriorityQueue<>(
-                map.size(), 
-                (a, b) -> b.getValue().compareTo(a.getValue())
-            ); 
+        int n = map.size();
+        int[] unique = new int[n]; 
         
-        
-        for(Map.Entry<Integer,Integer> e : map.entrySet()) { 
-            q.offer(e);
+        int i = 0;
+        for (int num: map.keySet()) {
+            unique[i] = num;
+            i++;
         }
         
-        int[] ans = new int[k];
+        quickselect(0, n-1, n-k, unique, map);
+        return Arrays.copyOfRange(unique, n - k, n);
+    }
+}
+
+
+
+
+class Solution {
+    public int[] topKFrequent(int[] nums, int k) {      
+        Map<Integer, Integer> map = new HashMap<>(); 
         
-        for (int i=k-1; i>=0; i--) { 
-            ans[i] = q.poll().getKey();
+        for (int i : nums) { 
+            map.compute(i, (n, f) -> f != null ? ++f : 1);
+        }
+        
+        Queue<int[]> pq = new PriorityQueue<>(k+1, (a, b) -> b[1] - a[1]);
+        
+        for (Map.Entry<Integer, Integer> e : map.entrySet()) { 
+            pq.add(new int[]{ e.getKey(), e.getValue() });
+
+            // need to be min heap instead of max heap
+            // if (pq.size() > k) pq.poll();
+        }
+        
+        int[] ans = new int[k]; 
+        
+        for (int i=0; i<k; i++) { 
+            ans[i] = pq.poll()[0]; 
         }
         
         return ans; 
     }
 }
+
+
