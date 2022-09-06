9fda17db72428ea7852c49d7077a309160e99420
mmajd <mabulmagd55@gmail.com>
2022-09-06 12:24:03 +0200

    (DP/Knapsack) basick knapsack top-down approach

--- dp/KnapsackTopDown.java
+++ dp/KnapsackTopDown.java
@@ -12,15 +12,61 @@
 
  */
 
+/** With memo */
+class Knapsack {
+    private int solve(int[] p, int[] w, int c, int idx, Integer[][]dp) {
+        if (c <= 0 || idx >= p.length) return 0;
+        if (dp[c][idx] != null) return dp[c][idx]; 
+
+        int p1 = 0; 
+        if (w[idx] <= c) {
+            p1 = p[idx] + solve(p, w, c-w[idx], idx+1);
+        }
+
+        int p2 = solve(p, w, c, idx+1);
+
+        dp[c][idx] = Math.max(p1, p2); 
+
+        return dp[c][idx]; 
+    }
+
+    public int solveKnapsack(int[] p, int[] w, int c) {
+        return solve(p, w, c, 0, new Integer[c+1][p.length]); 
+    }
+
+    public static void main(String[] args) {
+        Knapsack ks = new Knapsack();
+        int[] profits = { 4, 5, 3, 7 };
+        int[] weights = { 2, 3, 1, 4 }; 
+        int maxProfit = ks.solveKnapsack(profits, weights, 5);
+        System.out.println("Total knapsack profit ---> " + maxProfit);
+        maxProfit = ks.solveKnapsack(profits, weights, 6);
+        System.out.println("Total knapsack profit ---> " + maxProfit);
+    }
+}
+
+
+
+
+
+/** Without memo */
 class Knapsack {
     private int solve(int[] p, int[] w, int c, int idx) {
         if (c <= 0 || idx >= p.length)
             return 0;
 
         int p1 = 0; 
-
         // You have two choice either to take the current item or 
         // to skip it 
+
+        /**
+         *                       A 
+         *          TakeA /             \ Leave A 
+         *
+         *             B                 B 
+         *                              ... 
+         *    TakeB  /  \ Leave B      .....
+         */
 
         if (w[idx] <= c) {
             p1 = p[idx] + solve(p, w, c-w[idx], idx+1);
