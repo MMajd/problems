adfdb4aed72cc49c7a99e9acc0d6151f81be84b6
mmajd <mabulmagd55@gmail.com>
2022-09-08 13:09:25 +0200

    (dfs/stack/binary-tree) binary tree inorder traversal

--- BinaryTreeInorderTraversal.java
+++ BinaryTreeInorderTraversal.java
@@ -1,3 +1,28 @@
+/*
+
+  @link https://leetcode.com/problems/binary-tree-inorder-traversal/solution/
+
+  Given the root of a binary tree, return the inorder traversal of its nodes' values.
+
+Example 1:
+    Input: root = [1,null,2,3]
+    Output: [1,3,2]
+
+Example 2:
+    Input: root = []
+    Output: []
+
+Example 3:
+    Input: root = [1]
+    Output: [1]
+
+Constraints:
+    The number of nodes in the tree is in the range [0, 100].
+    -100 <= Node.val <= 100
+ */
+
+
+
 /**
  *
  * Definition for a binary tree TreeNode.
@@ -16,6 +41,31 @@
  */
 class Solution {
     public List<Integer> inorderTraversal(TreeNode root) {
+        return inorderIterative(root); 
+    }
+
+    private List<Integer> inorderIterative() { 
+        List<Integer> ans = new ArrayList<>(); 
+        Deque<TreeNode> stack = new LinkedList<>();
+        
+        TreeNode curr = root; 
+        
+        while(curr != null || !stack.isEmpty()) {
+            while (curr != null) { 
+                stack.push(curr);
+                curr = curr.left; 
+            }
+            
+            curr = stack.pop();
+            ans.add(curr.val);
+            curr = curr.right; 
+        }
+        
+        return ans;
+    }
+
+
+    private List<Integer> inorderDFS() { 
         Set<TreeNode> visited = new HashSet<>();
         Stack<TreeNode> stack = new Stack<>();
         List<Integer> ans = new ArrayList<>();
@@ -40,6 +90,7 @@ class Solution {
         
        return ans; 
     }
+
     
     public void inorder(TreeNode TreeNode, List<Integer> list) {
         if (TreeNode == null) return;
