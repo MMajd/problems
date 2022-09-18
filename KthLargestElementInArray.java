5bda7b37c3d15c95ade3c18cc7409578c5952db0
mmajd <mabulmagd55@gmail.com>
2022-09-18 03:05:40 +0200

    (quick-select/heap/priority-queue) find the kth largest element in an array

--- KthLargestElementInArray.java
+++ KthLargestElementInArray.java
@@ -1,16 +1,71 @@
+/* 
+  @link leetcode.com/problems/kth-largest-element-in-an-array
 
+  Given an integer array nums and an integer k, return the kth largest element in the array.
+Note that it is the kth largest element in the sorted order, not the kth distinct element.
 
-/** 
- *
- * @link leetcode.com/problems/kth-largest-element-in-an-array
- *
- * Java solution
+*********
+* NOTE  You must solve it in O(n) time complexity.
+*********
+
+Example 1:
+    Input: nums = [3,2,1,5,6,4], k = 2
+    Output: 5
+
+Example 2:
+    Input: nums = [3,2,3,1,2,4,5,5,6], k = 4
+    Output: 4
+ 
+Constraints:
+    1 <= k <= nums.length <= 105
+    -104 <= nums[i] <= 104
+  
  */
 
+/** Solution 1: Using quick select */
+class Solution {
+    public int findKthLargest(int[] nums, int k) {
+        return quickselect(nums, 0, nums.length-1, nums.length-k);
+    }
+    
+    private int quickselect(int[] nums, int left, int right, int k) {
+        int paritionIdx = parition(nums, left, right);
+        
+        if (paritionIdx > k) {
+            return quickselect(nums, left, paritionIdx-1, k);
+        }
+        
+        if (paritionIdx < k) { 
+            return quickselect(nums, paritionIdx+1, right, k);
+        }
+        
+        return nums[k]; 
+    }
+    
+    private int parition(int nums[], int left, int right) {
+        int pivot = nums[right]; 
+        
+        int j=left;
+        for (int i=left; i<right; i++) { 
+            if (pivot > nums[i]) { 
+                swap(nums, i, j++);
+            }
+        }
+        
+        swap(nums, right, j);
+        return j; 
+    }
+    
+    private void swap(int[] arr, int i, int j) {
+        int temp = arr[i]; 
+        arr[i] = arr[j]; 
+        arr[j] = temp; 
+    }
+}
+
+
 class KthLargestElementInArray { 
-    /** BETTER SPACE COMPLEXITY, O(K) */
     public int findKthLargest(int[] nums, in k) {
-        /** Min queue */
         PriorityQueue<Integer> q = new PriorityQueue<>(k+1);
 
         for (int i : nums) { 
@@ -22,9 +77,7 @@ class KthLargestElementInArray {
         return q.peek();
     }
 
-    /** SPACE OF O(n), First try solution */
     public int findKthLargest(int[] nums, int k) {
-        /** Max Queue */
         PriorityQueue<Integer> q = new PriorityQueue<>(
             k,
             (a, b) -> b.compareTo(a)
