9673679b66c07eff25a1159718da98a3e2b6417a
mmajd <mabulmagd55@gmail.com>
2022-09-18 13:31:36 +0200

    (ordered-set/tree/priority-queue/line-sweep) the skyline problem

--- SkylineProblem.java
+++ SkylineProblem.java
@@ -1,5 +1,8 @@
 /*
+
  @link https://leetcode.com/problems/the-skyline-problem
+   
+ @categories (ordered-set/tree/priorirty-queue/line-sweep)
 
  A city's skyline is the outer contour of the silhouette formed by all the buildings in that city when viewed from a distance. Given the locations and heights of all the buildings, return the skyline formed by these buildings collectively.
 
