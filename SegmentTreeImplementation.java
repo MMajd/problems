e83096581224d9c6748798cf07ae86de3d45e400
mmajd <mabulmagd55@gmail.com>
2022-09-21 05:09:53 +0200

    (segment-tree) update seg tree imp

--- SegmentTreeImplementation.java
+++ SegmentTreeImplementation.java
@@ -1,149 +1,122 @@
-
-/** Just to explain what I mean buy lazy operation functional interface*/
-
-@FunctionalInterface
-interface TernaryOperator<A> {
-    A apply(A a, A b, A c);
-
-    default <A> TernaryOperator<A> andThen(Function<? super A, ? extends A> after) {
-        Objects.requireNonNull(after);
-        return (A a, A b, A c) -> after.apply(apply(a, b, c));
-    }
-}
-
-
 /** 
-This is an implementation to Segment Tree class 
- */
+ *
+ * Segment Tree implementation in form of 
+ * Range Sum Query
+ *
+ */ 
 
-class SegmentTree { 
-    private int[] seg, lazy, data; 
-    private int length; 
+class NumArray {
+    private int[] seg, arr; 
+    private int n; 
 
-    BinaryOperator<Integer> operation; 
-    TriFucntion<Integer> lazyOperation; 
+    public NumArray(int[] nums) {
+        n = nums.length; 
+        seg = new int[4*n]; arr = nums.clone(); 
+        build(1, 0, n-1); }
 
-
-    // opreation, lazy operation area functional interfaces, to get a callback function 
-    // the do the required operation what ever it could be (ex: sum, min, max...etc)
-    // lazy operation is to do the lazy propogation 
-    public SegmentTree(T[] data, BinaryOperator<Integer> operation, TernaryOperator<Integer> lazyOperation) { 
-        length = data.length * 4;  // we can suffices with n log n as its already a binary tree
-        seg = new int[length]; 
-        lazy = new int[lenght];
-
-        this.data = data.clone(); 
-
-        this.operation = operation; 
-        this.lazyOperation = lazyOperation; 
-        
-        build(0, 0, data.length-1); 
-    }
-
-    /** 
-        Note: almost all segment tree functions will need to know which position we are working on 
-        the range we are considering, every range has a start and end point inclusive 
-    */
     private void build(int pos, int start, int end) { 
-        propogateIfNeeded(pos, start, end); 
-
-        // if we are left with only one element then update the seg tree pos 
         if (start == end) { 
-            seg[pos] = data[start]; 
-            return;
+            seg[pos] = arr[start]; return; 
         }
-
-        int mid = start + (end - start) / 2; 
+        
+        int mid = start+(end-start)/2; 
+        
         build(2*pos, start, mid); 
         build(2*pos+1, mid+1, end); 
-
- 
-        seg[pos] = operation(seg[2*pos], seg[2*pos+1]); 
+        
+        seg[pos] = seg[2*pos] + seg[2*pos+1]; 
     }
-
-    private void update(int pos, int start, int end, int idx, int val) { 
-        propogateIfNeeded(pos, start, end); 
-
+    
+    private void updateHelper(int pos, int start, int end, 
+                              int idx, int val) { 
+        
         if (start == end) { 
-            seg[pos] = val; 
-            return;
+            seg[pos] = val; return; 
         }
-
-        int mid = start + (end-start) / 2; 
-
-        if (mid > pos) update(pos, mid+1, end, idx, val); 
-        else update(pos, start, mid, idx, val); 
-
-        seg[pos] = operation(seg[2*pos], seg[2*pos+1]); 
+        
+        int mid = start+(end-start)>>1;
+        
+        if (idx>mid) updateHelper(2*pos+1, mid+1, end, idx, val); 
+        else updateHelper(2*pos, start, mid, idx, val); 
+        
+        seg[pos] = seg[2*pos] + seg[2*pos+1]; 
     }
-
-    /*update range, here we use lazy propogation to not go deep in the tree until we really need to do so*/
-    private void update(int pos, int start, int end, int updateRangeStart, int updateRangeEnd, int val) { 
-        propogateIfNeeded(pos, start, end); 
-
-        /*  meaning that the range given contains the current seg tree range 
-                      a, b                          s, e 
-            ex: range(3, 8) contains seg tree range(4, 7)
-            condition is a<=s && b>= e
-            a: updateRangeStart, b: updateRangeEnd, s: start, e: end
-        */
-        if (start>=updateRangeStart && end<=updateRangeEnd) { 
-            // could be adding some number or something like that
-            // here lazyOperation need to handle range also
-            int rangeLength = end-start+1; 
-            seg[pos] = lazyOperation(seg[pos], val, rangeLength); 
-            if (start != end) { 
-                lazy[2*pos] = lazyOperation(seg[pos], val, 1); 
-                lazy[2*pos+1] = lazyOperation(seg[pos], val, 1); 
-            }
-
-            return;
-        }
-
-        // update range is completely out of this seg tree range
-        if (start>=updateRangeEnd || end<=updateRangeStart) return;
-
-        int mid = start + (end-start) / 2; 
-
-        update(pos, start, mid, updateRangeStart, updateRangeEnd, val)
-        update(pos, mid+1, end, updateRangeStart, updateRangeEnd, val)
-
-        seg[pos] = operation(seg[2*pos], seg[2*pos+1]);
+    
+    public void update(int index, int val) {
+        updateHelper(1, 0, n-1, index, val); 
     }
-
-
-    private void propogateIfNeeded(int pos, int start, int end) { 
-        if (lazy[pos] != 0) { 
-            int rangeLength = end-start+1;
-            seg[pos] = lazyOperation(seg[pos], lazy[pos], rangeLength); 
-
-            if (start != end) { 
-                lazy[2*pos] = lazyOperation(seg[pos], val, 1); 
-                lazy[2*pos+1] = lazyOperation(seg[pos], val, 1); 
-            }
-
-            lazy[pos] = 0; 
+    
+    
+    public int get(int pos, int start, int end, int left, int right) { 
+        if (start>=left && end<=right) { 
+            return seg[pos]; 
         }
-    }
-
-    private int get(int pos, int start, int end, int queryRangeStart, int queryRangeEnd) { 
-        propogateIfNeeded(pos, start, end); 
-
-        if (start>=queryRangeStart && end<=queryRangeEnd) { 
-            return seg[pos]
-        }
-
-        if (start<=queryRangeEnd || end>=queryRangeStart) {
-            return 0;  // TODO: change this is value according to problem need
-        }
-
+        
+        if (start>right || end<left) return 0; 
+        
         int mid = start+(end-start)/2; 
-
-        int left = get(pos, start, mid, queryRangeStart, queryRangeEnd); 
-        int right = get(pos, mid+1, end, queryRangeStart, queryRangeEnd); 
-
-        return operation(left, right); 
+        int l = get(2*pos, start, mid, left, right); 
+        int r = get(2*pos+1, mid+1, end, left, right); 
+        
+        return l+r; 
+    }
+    
+    public int sumRange(int left, int right) {
+        return get(1, 0, n-1, left, right); 
     }
 }
 
+
+class NumArray {
+    int[] tree;
+    int treelen; 
+    int datalen; 
+    int n; 
+    int h; 
+
+    public NumArray(int[] nums) {
+        datalen = nums.length;
+        h = (int)Math.ceil((Math.log(datalen)/Math.log(2))); 
+        n = 1<<h; 
+        treelen = (n<<1); // complete binary tree height (1 << (h+1))
+        tree = new int[treelen];
+        build(nums);
+    }
+
+    private void build(int[] data) {
+        for (int i=0; i<datalen; i++) {
+            tree[i+n] = data[i]; 
+        }
+        
+        for (int i=n-1; i>=1; --i) { 
+            tree[i] = tree[i<<1] + tree[(i<<1)+1];
+        }
+    }
+
+    public void update(int idx, int val) {
+        tree[idx+n] = val; 
+        
+        for (int i=(n+idx)>>1; i>=1; i=i>>1) { 
+            tree[i] = tree[i<<1] + tree[(i<<1)+1];
+        }
+    }
+
+    private int execQuery(int pos, int low, int high, int qrylow, int qryhigh) {
+        if(low >= qrylow && high<=qryhigh)
+            return tree[pos];
+
+        if(high<qrylow || low>qryhigh)
+            return 0;
+
+        int mid = low + ((high - low)>>1);  
+        int l = execQuery(pos<<1, low, mid, qrylow, qryhigh);
+        int r = execQuery((pos<<1)+ 1, mid + 1, high, qrylow, qryhigh);
+        
+        return l+r;
+    }
+
+    public int sumRange(int qrylow, int qryhigh) {
+        return execQuery(1, 0, n-1, qrylow, qryhigh);
+    }
+}
 
