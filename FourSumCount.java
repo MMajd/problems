a03c8baab32306eede715af7ac96205f89000ac8
mmajd <mabulmagd55@gmail.com>
2022-10-09 04:44:59 +0200

    (hash-table/arrays) four sum II, find four sum count in 4 arrays

--- FourSumCount.java
+++ FourSumCount.java
@@ -1,6 +1,7 @@
 /*
 
  @link https://leetcode.com/problems/4sum-ii/
+ @categories (hash-table)
 
  Given four integer arrays nums1, nums2, nums3, and nums4 all of length n, return the number of tuples (i, j, k, l) such that:
 
