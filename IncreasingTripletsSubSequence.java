c1956e57548fbfe10193cbc5266663998d5b4e1b
mmajd <mabulmagd55@gmail.com>
2022-10-11 03:55:33 +0200

    (greedy/patient-sort) find if there"s an increasing triplets subsequences

--- IncreasingTripletsSubSequence.java
+++ IncreasingTripletsSubSequence.java
@@ -1,6 +1,33 @@
-/** 
- *
- */
+/* 
+ @link https://leetcode.com/problems/increasing-triplet-subsequence/
+ @categories (arrays/greedy/patience-sort)
+
+ Given an integer array nums, return true if there exists a triple of indices (i, j, k) 
+ such that i < j < k and nums[i] < nums[j] < nums[k]. If no such indices exists, return false.
+
+Example 1:
+    Input: nums = [1,2,3,4,5]
+    Output: true
+    Explanation: Any triplet where i < j < k is valid.
+
+Example 2:
+    Input: nums = [5,4,3,2,1]
+    Output: false
+    Explanation: No triplet exists.
+
+Example 3:
+    Input: nums = [2,1,5,0,4,6]
+    Output: true
+    Explanation: The triplet (3, 4, 5) is valid because nums[3] == 0 < nums[4] == 4 < nums[5] == 6.
+
+Constraints:
+    1 <= nums.length <= 5 * 105
+    -231 <= nums[i] <= 231 - 1
+ 
+
+Follow up: 
+    Could you implement a solution that runs in O(n) time complexity and O(1) space complexity?
+*/
 
 class IncreasingTripletsSubSequence {
     /** n solution */
@@ -47,12 +74,9 @@ class IncreasingTripletsSubSequence {
         int len = nums.length; 
         if (len <= 2) return false; 
         
-        int[] dp = new int[len]; 
-        
         TreeSet<Integer> set = new TreeSet<>();
         
         // Patience sort nlogn  
-        
         for (int i=0; i<len; i++) { 
             Integer x = set.ceiling(nums[i]);
             
