032974df12be825844c86170227a065d2ad7abea
mmajd <mabulmagd55@gmail.com>
2022-10-26 15:19:31 +0200

    (sliding-window/hash-table/string) minimum window substring

--- MinWindowSubString.java
+++ MinWindowSubString.java
@@ -1,56 +1,79 @@
-/** 
- *
- * @link https://leetcode.com/problems/minimum-window-substring/
- *
- *
- * Sliding Window
- *
+/* 
+ @link https://leetcode.com/problems/minimum-window-substring/
+ @categories (sliding-window/hash-table/string) 
+
+ Given two strings s and t of lengths m and n respectively, return the minimum window substring of s such that every character in t (including duplicates) is included in the window. If there is no such substring, return the empty string "".
+
+The testcases will be generated such that the answer is unique.
+
+A substring is a contiguous sequence of characters within the string.
+
+Example 1:
+    Input: s = "ADOBECODEBANC", t = "ABC"
+    Output: "BANC"
+    Explanation: The minimum window substring "BANC" includes 'A', 'B', and 'C' from string t.
+
+Example 2:
+    Input: s = "a", t = "a"
+    Output: "a"
+    Explanation: The entire string s is the minimum window.
+
+Example 3:
+    Input: s = "a", t = "aa"
+    Output: ""
+    Explanation: Both 'a's from t must be included in the window.
+    Since the largest window of s only has one 'a', return empty string.
+
+Constraints:
+    m == s.length
+    n == t.length
+    1 <= m, n <= 105
+    s and t consist of uppercase and lowercase English letters.
  */
 
-class MinWindowSubString {
+class Solution {
     public String minWindow(String s, String t) {
-        if (t.length() == 0) return ""; 
+        int m = s.length(); 
+        int n = t.length(); 
         
-        int start = -1, end = -1; 
-        int j = 0, minLen = Integer.MAX_VALUE; 
-        Map<Character, Integer> smap = new HashMap<>(); 
-        Map<Character, Integer> tmap = new HashMap<>(); 
+        Map<Character, Integer> smap = new HashMap<>();
+        Map<Character, Integer> tmap = new HashMap<>();
         
-        for (int i=0; i<t.length(); i++) { 
-            char c = t.charAt(i); 
-            tmap.put(c, 1+tmap.getOrDefault(c, 0));
+        for (int i=0; i<n; i++) { 
+            tmap.compute(t.charAt(i), (k, v) -> v == null ? 1 : ++v);
         }
         
-        int available = 0;  
-        int required = tmap.size();
+        int j = 0; 
+        int have = 0, need = tmap.size();
+        int start=-1, end=-1, minLen = Integer.MAX_VALUE; 
         
-        for (int i=0; i<s.length(); i++)  { 
-            char c = s.charAt(i);
+        for (int i=0; i<m; i++) {
+            char x = s.charAt(i);
+            smap.compute(x, (k,v) -> v == null ? 1 : ++v);
             
-            smap.put(c, 1+smap.getOrDefault(c, 0));
             
-            if (tmap.containsKey(c) 
-                && smap.get(c).intValue() == tmap.get(c).intValue()) available += 1; 
+            if (tmap.containsKey(x) && smap.get(x).intValue() == tmap.get(x).intValue()) {
+                have += 1; 
+            }
             
-            while (available == required) { 
+            while (have == need) { 
                 int size = i-j+1; 
                 
                 if (size < minLen) { 
+                    minLen = size; 
                     start = j; 
                     end = i; 
-                    minLen = size; 
                 }
                 
-                char mostLeft = s.charAt(j++);
-                smap.put(mostLeft, smap.get(mostLeft)-1);
+                char y = s.charAt(j++);
+                smap.compute(y, (k, v) -> --v);
                 
-                if (tmap.containsKey(mostLeft) 
-                    && smap.get(mostLeft).intValue() < tmap.get(mostLeft).intValue()) { 
-                    available -= 1; 
+                if (tmap.containsKey(y) && smap.get(y).intValue() < tmap.get(y).intValue()) { 
+                    have -= 1;
                 }
             }
         }
         
-        return minLen == Integer.MAX_VALUE? "" : s.substring(start, end+1);
+        return minLen == Integer.MAX_VALUE ? "" : s.substring(start, end+1);
     }
 }
