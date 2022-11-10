280ef955d99f5004282b883fba57907169418f0d
mmajd <mabulmagd55@gmail.com>
2022-11-10 08:17:28 +0200

    (dp/binary-search) split array largest sum

--- SplitArrayLargestSum.java
+++ SplitArrayLargestSum.java
@@ -30,6 +30,10 @@ Constraints:
     1 <= m <= min(50, nums.length)
 */
 
+/**************************************/
+/*      2. DP SOLUTIONS BELOW         */
+/**************************************/
+
 class Solution {
     public int splitArray(int[] A, int m) {
         int left= 0, right=0;
@@ -56,5 +60,68 @@ class Solution {
         }
         
         return left; 
+    }
+}
+
+class Solution2 {
+    public int splitArray(int[] nums, int m) {
+        int n = nums.length;
+        
+        int[] presum = new int[n+1];
+        for (int i=0; i<n; i++) {
+            presum[i+1] = presum[i] + nums[i];
+        }
+        
+        int[][] dp = new int[n+1][m+1];
+        Arrays.setAll(dp, i -> { 
+            Arrays.fill(dp[i], Integer.MAX_VALUE);
+            return dp[i]; 
+        });
+        
+        dp[0][0] = 0; 
+
+        for (int i=1; i<=n; i++) { 
+            for (int j=1; j<=Math.min(i, m); j++) { 
+                for (int k=0; k<i; k++) { 
+                    dp[i][j] = Math.min(Math.max(dp[k][j-1], presum[i]-presum[k]), dp[i][j]);
+                }
+            }
+        }
+        
+        return dp[n][m]; 
+    }
+}
+
+
+
+class Solution3 {
+    public int splitArray(int[] nums, int m) {
+        int n = nums.length;
+        
+        int[] presum = new int[n + 1];
+        for (int i=0; i<n; i++) {
+            presum[i+1] = presum[i] + nums[i];
+        }
+        
+        // dp[i][j]: result for partition nums[0:i] into j part (for i in [0, n-1])
+        // notice that partition into 0 part is not defined, namely dp[i][0] is never used for all i in [0,n-1]
+        int[][] dp = new int[n][m+1];
+        
+        for (int i = 0; i < n; i++) Arrays.fill(dp[i], Integer.MAX_VALUE);
+        
+        for (int i=0; i<n; i++) {
+            dp[i][1] = presum[i+1];
+        }
+        
+        for (int i=0; i<n; i++) {
+            for (int j=2; j<=Math.min(m, i+1); j++) {
+                // [0,i] into j part  <==> [0,k] into j-1 part  &  [k+1, i] into 1 part  for k in [0,i-1]
+                for (int k=0; k<i; k++) {
+                    dp[i][j] = Math.min(dp[i][j], Math.max(dp[k][j-1], presum[i+1] - presum[k+1]));
+                }
+            }
+        }
+     
+        return dp[n-1][m];        
     }
 }
