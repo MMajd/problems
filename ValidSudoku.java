d624818eebb62b81205b61f9c4580dbd74a9de39
mmajd <mabulmagd55@gmail.com>
2022-11-23 08:45:10 +0200

    (mapping/matrix/math) check of given sudoku board is valid or not

--- ValidSudoku.java
+++ ValidSudoku.java
@@ -47,21 +47,22 @@ Constraints:
 
  */ 
 
-
 class Solution {
     public boolean isValidSudoku(char[][] board) {
         int[][] rows = new int[9][9]; 
         int[][] cols = new int[9][9]; 
-        // treat boxes as rows each row has numbers boxes for 1 - 9
         int[][] boxes = new int[9][9]; 
         
         for (int i=0; i<9; i++) { 
             for (int j=0; j<9; j++) {
                 if (board[i][j] == '.') continue; 
                 int num = board[i][j]-'1'; 
-
+                
+                // assume we have 3x3 matrix and we want to convert it to be 1-d array of size 9
+                // during mapping of each cell we multiple the row by number of columns and 
+                // then add the current col to reach that cell 
                 int boxIdx = ((i/3) * 3) + (j/3); 
-
+                
                 if (rows[i][num] != 0 
                    || cols[j][num] != 0
                    || boxes[boxIdx][num] != 0) return false; 
