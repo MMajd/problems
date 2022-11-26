cfd01f0ca6805f4533f472381daba984ae0e3473
mmajd <mabulmagd55@gmail.com>
2022-11-26 17:36:28 +0200

    (dp/monotonic-stack) sum of subarray minimums

--- dp/SumOfSubarrayMinimums.java
+++ dp/SumOfSubarrayMinimums.java
@@ -2,6 +2,8 @@
  @link https://leetcode.com/problems/sum-of-subarray-minimums
  @categories (dp/monotonic-stack/)
 
+ @help https://youtu.be/fDeZNRNmm1Y
+
  Given an array of integers arr, find the sum of min(b), where b ranges over every (contiguous) subarray of arr. Since the answer may be large, return the answer modulo 109 + 7.
 
 Example 1:
@@ -15,32 +17,35 @@ Example 1:
 Example 2:
     Input: arr = [11,81,94,43,3]
     Output: 444
- 
 
 Constraints:
     1 <= arr.length <= 3 * 10^4
     1 <= arr[i] <= 3 * 10^4
 */
+
 class Solution {
     final int MOD = 1000_000_007; 
 
-    public int sumSubarrayMins(int[] arr) {
-        int[] stack = new int[arr.length + 1];
-        int idxStack = 0;
+    public int sumSubarrayMins(int[] A) {
+        int N = A.length;
+        int top = 0; 
+        int[] stack = new int[N]; 
 
-        int[] dp = new int[arr.length];
-        dp[0] = arr[0];
+        int[] dp = new int[N]; 
+        dp[0] = A[0];
 
-        long res = dp[0];
-        for (int i = 1; i < arr.length; i++) {
-            while (idxStack >= 0 && arr[stack[idxStack]] >= arr[i]) idxStack--; //pop
-            dp[i] = idxStack < 0 ? 
-                        arr[i] * (i + 1) : 
-                        dp[stack[idxStack]] + arr[i] * (i - stack[idxStack]);
-            res += dp[i];
-            stack[++idxStack] = i; //push
+        long ans = dp[0]; 
+
+        for (int i=1; i<N; i++) {
+            while(top >= 0 && A[stack[top]] >= A[i]) top--; // poping
+
+            if (top >= 0) dp[i] = dp[stack[top]] + A[i] * (i-stack[top]); 
+            else dp[i] = A[i] * (i+1); 
+
+            ans = (ans + dp[i]) % MOD; 
+            stack[++top] = i; 
         }
-
-        return (int) (res % MOD);
+        
+        return (int)(ans % MOD); 
     }
 }
