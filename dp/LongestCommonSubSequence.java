eab1f00f36bc22b1936a30f3701f54ea1ef468d3
mmajd <mabulmagd55@gmail.com>
2022-12-15 19:15:32 +0200

    (dp/string/arrays) find longest common subsequence between two strings

--- dp/LongestCommonSubSequence.java
+++ dp/LongestCommonSubSequence.java
@@ -116,30 +116,29 @@ class Solution {
 
 class LongestCommonSubSequence {
     public int longestCommonSubsequence(String text1, String text2) {
-        
         return solve(text1, text2);
-        
     }
-    
-    public int solve(String w1, String w2) { 
-        int len1 = w1.length(); 
-        int len2 = w2.length(); 
-        int max = 0; 
-        
-        int[][] dp = new int[len1+1][len2+1]; 
-        for (int i=0; i<len1; i++) { 
-            for (int j=0; j<len2; j++) { 
-                if (w1.charAt(i) == w2.charAt(j)) { 
-                    dp[i+1][j+1] = dp[i][j] + 1; 
-                } 
-                else { 
-                    dp[i+1][j+1] = Math.max(dp[i][j+1], dp[i+1][j]);
+
+    public int solve (String s1, String s2) {
+        int m = s1.length(); 
+        int n = s2.length(); 
+
+        int[][] dp = new int[m+1][n+1]; 
+
+        char[] S1 = s1.toCharArray(); 
+        char[] S2 = s2.toCharArray(); 
+
+        for(int i=0; i<m; i++) { 
+            for (int j=0; j<n; j++) { 
+                if (S1[i] == S2[j]) { 
+                    dp[i+1][j+1] = 1 + dp[i][j];
                 }
-                
-                max = Math.max(max, dp[i+1][j+1]);
+                else { 
+                    dp[i+1][j+1] = Math.max(dp[i+1][j], dp[i][j+1]);
+                }
             }
         }
-        
-        return max; 
+
+        return dp[m][n]; 
     }
 }
