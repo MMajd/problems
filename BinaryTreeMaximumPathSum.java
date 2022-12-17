c8e35c9ba74776591766fb7279e652eebbe67849
mmajd <mabulmagd55@gmail.com>
2022-12-17 10:48:03 +0200

    (dfs/dp) binary tree max path sum

--- BinaryTreeMaximumPathSum.java
+++ BinaryTreeMaximumPathSum.java
@@ -26,34 +26,22 @@ Constraints:
 
 */
 
+
 class Solution {
+    int ans = Integer.MIN_VALUE; 
+    
     public int maxPathSum(TreeNode root) {
-        maxSum = Integer.MIN_VALUE;
-        gainFromSubtree(root);
-        return maxSum;
+        dfs(root);
+        return ans; 
     }
 
-    private int maxSum;
-
-    // post order traversal of subtree rooted at `root`
-    private int gainFromSubtree(TreeNode root) {
-        if (root == null) {
-            return 0;
-        }
-
-        // add the path sum from left subtree. Note that if the path
-        // sum is negative, we can ignore it, or count it as 0.
-        // This is the reason we use `Math.max` here.
-        int gainFromLeft = Math.max(gainFromSubtree(root.left), 0);
-
-        // add the path sum from right subtree. 0 if negative
-        int gainFromRight = Math.max(gainFromSubtree(root.right), 0);
-
-        // if left or right path sum are negative, they are counted
-        // as 0, so this statement takes care of all four scenarios
-        maxSum = Math.max(maxSum, gainFromLeft + gainFromRight + root.val);
-
-        // return the max sum for a path starting at the root of subtree
-        return Math.max(gainFromLeft + root.val, gainFromRight + root.val);
+    private int dfs(TreeNode root) {
+        if (root == null) return 0;
+        int l = Math.max(dfs(root.left), 0); 
+        int r = Math.max(dfs(root.right), 0); 
+        int v = root.val; 
+        ans = Math.max(l+r+v, ans);
+        return Math.max(l+v, r+v);
     }
 }
+
