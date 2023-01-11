d036d95e2518ae0592c01886da733315c434aeb8
mmajd <mabulmagd55@gmail.com>
2023-01-11 21:04:26 +0200

    (tree/dfs) Minimum Time to Collect All Apples in a Tree

---
+++ MinimumTimeToCollectAllApplesInATree.java
@@ -0,0 +1,59 @@
+/*
+ @link https://leetcode.com/problems/minimum-time-to-collect-all-apples-in-a-tree
+ @categories (tree/dfs)
+
+ Given an undirected tree consisting of n vertices numbered from 0 to n-1, which has some apples in their vertices. You spend 1 second to walk over one edge of the tree. Return the minimum time in seconds you have to spend to collect all apples in the tree, starting at vertex 0 and coming back to this vertex.
+
+The edges of the undirected tree are given in the array edges, where edges[i] = [ai, bi] means that exists an edge connecting the vertices ai and bi. Additionally, there is a boolean array hasApple, where hasApple[i] = true means that vertex i has an apple; otherwise, it does not have any apple.
+
+Example 1:
+    Input: n = 7, edges = [[0,1],[0,2],[1,4],[1,5],[2,3],[2,6]], hasApple = [false,false,true,false,true,true,false]
+    Output: 8 
+    Explanation: The figure above represents the given tree where red vertices have an apple. One optimal path to collect all apples is shown by the green arrows.  
+
+Example 2:
+    Input: n = 7, edges = [[0,1],[0,2],[1,4],[1,5],[2,3],[2,6]], hasApple = [false,false,true,false,false,true,false]
+    Output: 6
+    Explanation: The figure above represents the given tree where red vertices have an apple. One optimal path to collect all apples is shown by the green arrows.  
+
+Example 3:
+    Input: n = 7, edges = [[0,1],[0,2],[1,4],[1,5],[2,3],[2,6]], hasApple = [false,false,false,false,false,false,false]
+    Output: 0
+ 
+Constraints:
+    1 <= n <= 10^5
+    edges.length == n - 1
+    edges[i].length == 2
+    0 <= ai < bi <= n - 1
+    fromi < toi
+    hasApple.length == n
+*/
+
+class Solution {
+    public int minTime(int n, int[][] edges, List<Boolean> hasApple) {
+        Map<Integer,List<Integer>> adj = new HashMap<>(); 
+
+        for (int[] e : edges) { 
+            adj.computeIfAbsent(e[0], k -> new ArrayList<>()).add(e[1]);
+            adj.computeIfAbsent(e[1], k -> new ArrayList<>()).add(e[0]);
+        }
+
+        return dfs(0, -1, adj, hasApple);
+    }
+
+    private int dfs(int node, int parent, Map<Integer, List<Integer>> adj, List<Boolean> hasApple) {
+        if (!adj.containsKey(node)) return 0; 
+        int totalTime = 0;
+        int childTime = 0; 
+
+        for (int child : adj.get(node)) { 
+            if (child == parent) continue; 
+            childTime = dfs(child, node, adj, hasApple);
+            if (childTime > 0 || hasApple.get(child)) {
+                totalTime += childTime + 2; 
+            }
+        }
+
+        return totalTime; 
+    }
+}
