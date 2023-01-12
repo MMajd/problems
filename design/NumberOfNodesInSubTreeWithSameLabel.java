7ecf72463613a4dba1d08b066b30556b51b5f0f8
mmajd <mabulmagd55@gmail.com>
2023-01-12 12:18:30 +0200

    (tree/n-tree/dfs) find number of nodes in sub tree with same label

--- design/NumberOfNodesInSubTreeWithSameLabel.java
+++ design/NumberOfNodesInSubTreeWithSameLabel.java
@@ -34,6 +34,55 @@ Constraints:
     labels is consisting of only of lowercase English letters.
 */
 
+/**
+ * DFS Solution Below
+ */ 
+
+/** BFS Solution starting from leafs first and going up
+ * This one is coming first as its more interesting that DFS
+ * */
+class Solution {
+    public int[] countSubTrees(int n, int[][] edges, String labels) {
+        Map<Integer, Set<Integer>> adj = new HashMap<>();
+        for (int[] edge : edges) {
+            int a = edge[0], b = edge[1];
+            adj.computeIfAbsent(a, value -> new HashSet<Integer>()).add(b);
+            adj.computeIfAbsent(b, value -> new HashSet<Integer>()).add(a);
+        }
+
+        int[][] counts = new int[n][26];
+        Queue<Integer> q = new LinkedList<>();
+
+        for (int i = 0; i < n; i++) {
+            counts[i][labels.charAt(i) - 'a'] = 1;
+            if (i != 0 && adj.get(i).size() == 1) {
+                q.offer(i);
+            }
+        }
+
+        while (!q.isEmpty()) {
+            int node = q.poll();
+            int parent = adj.get(node).stream().findFirst().get();
+            adj.get(parent).remove(node);
+
+            for (int i = 0; i < 26; i++) {
+                counts[parent][i] += counts[node][i];
+            }
+
+            if (parent != 0 && adj.get(parent).size() == 1) {
+                q.offer(parent);
+            }
+        }
+
+        int[] ans = new int[n];
+        for (int i = 0; i < n; i++) {
+            ans[i] = counts[i][labels.charAt(i) - 'a'];
+        }
+        return ans;
+    }
+}
+
+/** DFS Solution */
 class Solution {
     public int[] countSubTrees(int n, int[][] edges, String labels) {
         Map<Integer, Character> idx = new HashMap<>(); 
