6693b459abe812bb4e2e698ff2a91b20b22d6504
mmajd <mabulmagd55@gmail.com>
2023-03-12 20:29:56 +0200

    (dac/merge-sort/heap) merge k sorted list

--- linkedlist/MergeKSortedList.java
+++ linkedlist/MergeKSortedList.java
@@ -1,20 +1,15 @@
 /*
-  
-@link https://leetcode.com/problems/merge-k-sorted-lists/
+ @link https://leetcode.com/problems/merge-k-sorted-lists/
+ @categories (d-and-c/heap/merge-sort) 
 
 You are given an array of k linked-lists lists, each linked-list is sorted in ascending order.
-
 Merge all the linked-lists into one sorted linked-list and return it.
 
 Example 1:
     Input: lists = [[1,4,5],[1,3,4],[2,6]]
     Output: [1,1,2,3,4,4,5,6]
     Explanation: The linked-lists are:
-    [
-      1->4->5,
-      1->3->4,
-      2->6
-    ]
+    [1->4->5, 1->3->4, 2->6]
     merging them into one sorted list:
     1->1->2->3->4->4->5->6
 
@@ -28,17 +23,13 @@ Example 3:
  
 Constraints:
     k == lists.length
-    0 <= k <= 104
+    0 <= k <= 10^4
     0 <= lists[i].length <= 500
-    -104 <= lists[i][j] <= 104
+    -10^4 <= lists[i][j] <= 10^4
     lists[i] is sorted in ascending order.
-    The sum of lists[i].length will not exceed 104.
+    The sum of lists[i].length will not exceed 10^4.
 */
 
-
-
-
-
 /**
  * Definition for singly-linked list.
  * public class ListNode {
@@ -49,20 +40,6 @@ Constraints:
  *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
  */
-
-
-/**
- * Definition for singly-linked list.
- * public class ListNode {
- *     int val;
- *     ListNode next;
- *     ListNode() {}
- *     ListNode(int val) { this.val = val; }
- *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
- * }
- */
-
-
 class Solution {
     public ListNode mergeKLists(ListNode[] lists) { 
         if (lists.length == 0) return null; 
@@ -106,8 +83,7 @@ class Solution {
     }
 }
 
-
-class Solution3 {
+class Solution {
     public ListNode mergeKLists(ListNode[] lists) {
         PriorityQueue<ListNode> q = new PriorityQueue<>((a, b) -> a.val - b.val); 
         
@@ -135,39 +111,42 @@ class Solution3 {
     }
 }
 
-
-class Solution2 {
+/** Merge sort solution */ 
+class Solution {
     public ListNode mergeKLists(ListNode[] lists) {
-        ListNode head = null; 
-        for (ListNode n : lists) { 
-            head = mergeTwoLists(head, n);
-        }
-        return head; 
+        return partition(lists, 0); 
+    }
+
+    private ListNode partition(ListNode[] lists, int i) { 
+        if (i >= lists.length) return null; 
+        ListNode root = partition(lists, i+1); 
+        root = merge(root, lists[i]); 
+        return root; 
     }
     
-    private ListNode mergeTwoLists(ListNode l1, ListNode l2) { 
-        if (l1==null || l2==null) 
-            return l1==null ? l2 : l1; 
-        ListNode p1=l1, p2=l2, head=l1, prev=null; 
-        if (l1.val > l2.val) { 
-            p1 = l2; 
-            p2 = l1; 
-            head = p1; 
+    private ListNode merge(ListNode n1, ListNode n2) { 
+        if (n1 == null || n2 == null) { 
+            return n1 == null ? n2 : n1; 
         }
-        
-        while (p1!=null && p2!=null) { 
-            if (p1.val>p2.val) { 
+        ListNode p1 = n1.val > n2.val ? n2 : n1; 
+        ListNode p2 = p1 == n1 ? n2 : n1; 
+        ListNode head = p1; 
+        ListNode prev = null; 
+
+        while (p1 != null && p2 != null) { 
+            if (p1.val > p2.val) { 
                 prev.next = p2; 
                 prev = prev.next; 
                 p2 = p2.next; 
                 prev.next = p1; 
-            }
-            else { 
+            } else { 
                 prev = p1; 
                 p1 = p1.next; 
             }
         }
-        if (p1 == null) prev.next = p2; 
+        if (p1 == null) {
+            prev.next = p2; 
+        }
         return head; 
     }
 }
