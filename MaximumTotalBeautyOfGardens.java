accdd68607d0f1cbbb632673bb7a9e4bd09f498e
mmajd <mabulmagd55@gmail.com>
2023-04-17 00:52:03 +0200

    (greedy) maximize total beauty of n gardens

--- MaximumTotalBeautyOfGardens.java
+++ MaximumTotalBeautyOfGardens.java
@@ -64,7 +64,8 @@ class Solution {
 
         long ans = 0;
         for (int c = 0, i = n - 1; c <= n; ++c) {
-            long remain = prefix[n] - prefix[n - c] + newFlowers - c * (long) target, min = 0;
+            long min = 0;
+            long remain = prefix[n] - prefix[n - c] + newFlowers - c * (long) target;
             if (0 > remain) break;
             i = Math.min(i, n - c - 1);
             while (0 <= i && (target <= flowers[i] 
