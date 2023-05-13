2450e76a09e236c46c2e027f0915ceb20aa8a6b3
mmajd <mabulmagd55@gmail.com>
2023-05-13 12:47:32 +0300

    (dp) min cost for tickets

--- MinimumCostForTickets.java
+++ MinimumCostForTickets.java
@@ -6,9 +6,10 @@
 The days of the year in which you will travel are given as an integer array days. 
 Each day is an integer from 1 to 365. Train tickets are sold in three different ways:
 
-- a 1-day pass is sold for costs[0] dollars,
-- a 7-day pass is sold for costs[1] dollars, and
-- a 30-day pass is sold for costs[2] dollars.
+* a 1-day pass is sold for costs[0] dollars,
+* a 7-day pass is sold for costs[1] dollars, 
+* a 30-day pass is sold for costs[2] dollars.
+ 
 The passes allow that many days of consecutive travel.
 
 For example, if we get a 7-day pass on day 2, then we can travel for 7 days: 2, 3, 4, 5, 6, 7, and 8.
@@ -39,26 +40,25 @@ Constraints:
     1 <= costs[i] <= 1000
 */
 
-
 import static java.lang.Math.*; 
 
 class Solution {
     public int mincostTickets(int[] days, int[] costs) {
-        // int n = 365; 
         int n = days[days.length-1]; 
         int[] dp = new int[n+1]; 
-        int[] map = new int[n+1]; 
+        boolean[] travel = new boolean[n+1]; 
+
         for (int day : days) { 
-            map[day] += 1; 
+            travel[day] = true;
         }
-        for (int i=1; i<=n; ++i) {
+
+        for (int i=1; i<=n; i++) {
             dp[i] = dp[i-1];
-            if (map[i] != 0) {
-                dp[i] = min(dp[i-1]+costs[0], min(dp[max(0,i-7)]+costs[1], dp[max(0, i-30)]+costs[2]));
+            if (travel[i]) {
+                dp[i] = min(costs[0] + dp[i-1], min(costs[1] + dp[max(i-7, 0)], costs[2] + dp[max(i-30, 0)]));
             }
         }
 
-        return dp[n];
+        return dp[n]; 
     }
 }
-
