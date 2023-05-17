b39f3de85a085cf63e32659385ec02bc62d85ebe
mmajd <mabulmagd55@gmail.com>
2023-05-18 00:14:36 +0300

    (dp) number of ways of cutting pizza

--- NumberOfWaysOfCuttingPizza.java
+++ NumberOfWaysOfCuttingPizza.java
@@ -1,39 +1,39 @@
 /*
- @link https://leetcode.com/problems/number-of-ways-of-cutting-a-pizza
+ @link https://leetcode.com/problems/number-of-ways-of-cutting-a-A
  @categories (dp/matrix)
 
- Given a rectangular pizza represented as a rows x cols matrix containing the following characters: 
+ Given a rectangular Pizza represented as a rows x cols matrix containing the following characters: 
 'A' (an apple) and '.' (empty cell) and given the integer k. 
-You have to cut the pizza into k pieces using k-1 cuts. 
+You have to cut the Pizza into k pieces using k-1 cuts. 
 
 For each cut you choose the direction: vertical or horizontal, 
-then you choose a cut position at the cell boundary and cut the pizza into two pieces. 
-If you cut the pizza vertically, give the left part of the pizza to a person. 
-If you cut the pizza horizontally, give the upper part of the pizza to a person. 
-Give the last piece of pizza to the last person.
+then you choose a cut position at the cell boundary and cut the Pizza into two pieces. 
+If you cut the Pizza vertically, give the left part of the Pizza to a person. 
+If you cut the Pizza horizontally, give the upper part of the Pizza to a person. 
+Give the last piece of Pizza to the last person.
 
-Return the number of ways of cutting the pizza such that each piece contains at least one apple. 
+Return the number of ways of cutting the Pizza such that each piece contains at least one apple. 
 Since the answer can be a huge number, return this modulo 10^9 + 7.
 
 Example 1:
-    Input: pizza = ["A..","AAA","..."], k = 3
+    Input: A = ["A..","AAA","..."], k = 3
     Output: 3 
-    Explanation: The figure above shows the three ways to cut the pizza. Note that pieces must contain at least one apple.
+    Note that pieces must contain at least one apple.
 
 Example 2:
-    Input: pizza = ["A..","AA.","..."], k = 3
+    Input: A = ["A..","AA.","..."], k = 3
     Output: 1
 
 Example 3:
-    Input: pizza = ["A..","A..","..."], k = 1
+    Input: A = ["A..","A..","..."], k = 1
     Output: 1
 
 Constraints:
     1 <= rows, cols <= 50
-    rows == pizza.length
-    cols == pizza[i].length
+    rows == A.length
+    cols == A[i].length
     1 <= k <= 10
-    pizza consists of characters 'A' and '.' only.
+    A consists of characters 'A' and '.' only.
 */
 
 /** 
@@ -44,37 +44,51 @@ Constraints:
     0  0  0  0
 */ 
 class Solution {
-    public int ways(String[] pizza, int k) {
-        int m = pizza.length, n = pizza[0].length();
+    public int ways(String[] A, int k) {
+        int m = A.length, n = A[0].length();
         Integer[][][] dp = new Integer[k][m][n];
-        /* preSum[r][c] is the total number of apples in pizza[r:][c:] */
+        /* preSum[r][c] is the total number of apples in A[r:][c:] */
         int[][] preSum = new int[m+1][n+1];
 
+        // presum we always want know if our next cut will have apples or not
+        // so we create presum with 0-padding on most left col & deepest row 
+        // Diagram: 
+        /** 
+         *  r,c    
+         *   _     ___________
+         *        |          |
+         *        |          |
+         *    ----------------
+         *    |   |          |
+         *    |___|__________|
+         **/
         for (int r = m - 1; r >= 0; r--) { 
             for (int c = n - 1; c >= 0; c--) { 
-                preSum[r][c] = preSum[r][c+1] + preSum[r+1][c] - preSum[r+1][c+1] + (pizza[r].charAt(c) == 'A' ? 1 : 0);
+                preSum[r][c] = preSum[r][c+1] + preSum[r+1][c] - preSum[r+1][c+1] + (A[r].charAt(c) == 'A' ? 1 : 0);
             }
         }
-
-        System.out.println(Arrays.deepToString(preSum));
 
         return dfs(m, n, k-1, 0, 0, dp, preSum);
     }
 
     int dfs(int m, int n, int k, int r, int c, Integer[][][] dp, int[][] preSum) {
-        if (preSum[r][c] == 0) return 0; /* if the remain piece has no apple -> invalid */
-        if (k == 0) return 1; /* found valid way after using k-1 cuts */
+        /* if the remain piece has no apple -> invalid */
+        if (preSum[r][c] == 0) return 0; 
+        /* found valid way after using k-1 cuts */
+        if (k == 0) return 1; 
         if (dp[k][r][c] != null) return dp[k][r][c];
         int ans = 0;
-        // cut in horizontal
+        // horizontal cut
         for (int nr = r + 1; nr < m; nr++)  {
-            if (preSum[r][c] - preSum[nr][c] > 0) { /* cut if the upper piece contains at least one apple */
+            /* cut if the upper piece contains at least one apple */
+            if (preSum[r][c] - preSum[nr][c] > 0) { 
                 ans = (ans + dfs(m, n, k - 1, nr, c, dp, preSum)) % 1_000_000_007;
             }
         }
-        // cut in vertical
+        // vertical cut
         for (int nc = c + 1; nc < n; nc++) {
-            if (preSum[r][c] - preSum[r][nc] > 0) { /* cut if the left piece contains at least one apple */
+            /* cut if the left piece contains at least one apple */
+            if (preSum[r][c] - preSum[r][nc] > 0) { 
                 ans = (ans + dfs(m, n, k - 1, r, nc, dp, preSum)) % 1_000_000_007;
             }
         }
@@ -86,13 +100,13 @@ class Solution {
     private static final int MOD = 1000_000_007; 
     private Integer[][][] memo;
     private Integer[][] rowmemo; 
-    private String[] pizza; 
+    private String[] A; 
     private int R, C; 
 
-    public int ways(String[] pizza, int k) {
-        this.pizza = pizza; 
-        R = pizza.length; 
-        C = pizza[0].length(); 
+    public int ways(String[] A, int k) {
+        this.A = A; 
+        R = A.length; 
+        C = A[0].length(); 
         memo = new Integer[R][C][k+1];
         rowmemo = new Integer[R+1][C+1]; 
 
@@ -101,7 +115,7 @@ class Solution {
 
     private int countApplesByRow(int x, int y) { /* we could add a memo here also */
         if (y == C) return 0;
-        return countApplesByRow(x, y + 1) + (pizza[x].charAt(y) == 'A' ? 1 : 0);
+        return countApplesByRow(x, y + 1) + (A[x].charAt(y) == 'A' ? 1 : 0);
     }
 
     private int countApple(int x, int y) {
