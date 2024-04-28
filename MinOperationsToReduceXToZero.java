2d1f5b7b2cc8e617792ad94165e1e887498914d8
mmajd <mabulmagd55@gmail.com>
2024-04-28 20:08:12 +0300

    (prefix-sum/hashmap) add prefix sum solution

--- MinOperationsToReduceXToZero.java
+++ MinOperationsToReduceXToZero.java
@@ -38,3 +38,42 @@ class Solution {
         return (subArrayLen == 0) ? -1 : arr.length - subArrayLen;  
     }
 }
+
+/** 
+ * Using PrefixSum 
+ * solution is similar to sliding window 
+ * we get size of window through getting the value of target 
+ * using the hashmap used to store prefix sum 
+ */ 
+class Solution {
+    public int minOperations(int[] arr, int x) {
+        int n = arr.length; 
+        int target = -1 * x; // inner arr sum xxx|INNER ARR|xx
+
+        for (int val : arr) { 
+            target += val; 
+        }
+
+        if (target <= 0) return target == 0 ? n : -1; 
+
+        Map<Long, Integer> map = new HashMap<>(); // prefix map
+
+        map.put(0L, -1); // 0 sum goes to -1 index, to become -(-1) in (a)
+
+        int ans = 0; 
+        long sum = 0; 
+
+        for (int i=0; i<n; i++) { 
+            sum += arr[i]; 
+            long diff = sum - target; 
+
+            if (map.containsKey(diff)) { // if diff exist update ans
+                ans = Math.max(ans, i - map.get(diff)); // step-(a)
+            }
+
+            map.put(sum, i); // add the sum to prefix map
+        }
+
+        return ans == 0 ? -1 : n-ans; 
+    }
+}
