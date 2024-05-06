81cacc416b5cdcd038dc4b6b80affb84632b6b23
mmajd <mabulmagd55@gmail.com>
2024-05-06 18:47:10 +0300

    (floyds-algo/binary-search/bit-manipulation) find the duplicate number

---
+++ FindTheDuplicateNumber.java
@@ -0,0 +1,78 @@
+/**
+ @link https://leetcode.com/problems/find-the-duplicate-number
+ @categories(slow-fast pointers,binary-search, bit-manipulation)
+  
+ Given an array of integers nums containing n + 1 integers where each integer is in the range [1, n] inclusive.
+
+There is only one repeated number in nums, return this repeated number.
+
+You must solve the problem without modifying the array nums and uses only constant extra space.
+
+Example 1:
+    Input: nums = [1,3,4,2,2]
+    Output: 2
+
+Example 2:
+    Input: nums = [3,1,3,4,2]
+    Output: 3
+
+Example 3:
+    Input: nums = [3,3,3,3,3]
+    Output: 3
+
+Constraints:
+    1 <= n <= 10^5
+    nums.length == n + 1
+    1 <= nums[i] <= n
+    All the integers in nums appear only once except for precisely one integer which appears two or more times.
+ 
+
+Follow up:
+    How can we prove that at least one duplicate number must exist in nums?
+    Can you solve the problem in linear runtime complexity?
+ */ 
+
+// slow-fast pointers (floyd's algorithm) 
+class Solution {
+    public int findDuplicate(int[] nums) {
+        int slow = 0, fast = 0; 
+
+        do { 
+            slow = nums[slow]; 
+            fast = nums[nums[fast]]; 
+        } while (slow != fast); 
+
+        int x = 0; 
+
+        while (slow != x) { 
+            slow = nums[slow]; 
+            x = nums[x]; 
+        }
+
+        return slow; 
+    }
+}
+
+// binary-search solution
+class Solution {
+    public int findDuplicate(int[] nums) {
+        int left = 0; 
+        int right = nums.length - 1; 
+
+        while (left <= right) { 
+            int mid = left + (right - left) / 2; 
+            int p = 0; 
+
+            for (int x : nums) { 
+                p += (mid >= x) ? 1 : 0; 
+            }
+
+            if (p <= mid) { 
+                left = mid + 1; 
+            }
+            else right = mid - 1; 
+        }
+
+        return left; 
+    }
+}
